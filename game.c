0777a09a902139cc56a83c74b97dfa53d4f9c3ff
sn <snevoori1@toromail.csudh.edu>
2024-12-05 22:37:13 +0000

    sathwika final commit

--- game.c
+++ game.c
@@ -15,11 +15,11 @@
 
 //place function prototypes here
 
+void Detectivegame();
+int randomOutcome();
+void displayClues(char *clues[], int cluesFound);
 
-
-
-
-int main(int argc, char *argv[])
+	int main(int argc, char *argv[])
 {
 	int choice = 0;
 	char name[30] = "bob";
@@ -63,6 +63,7 @@ int main(int argc, char *argv[])
 			{
 				puts("room5");
 				//Sathwika
+				Detectivegame();
 				break;
 			}
 			case 6:
@@ -326,5 +327,111 @@ int main(int argc, char *argv[])
 }
 
 //place functions here
+void Detectivegame() {
+    srand(time(NULL));
+    int choice;
+    int gameOver = 0;
+    int cluesFound = 0;
+    int mysterySolved = 0;
+    char *clues[5];  
 
+    printf("Welcome to the Detective Mystery Game!\n");
+    printf("You are a detective solving a robbery case that went terribly wrong.\n");
+    printf("Your goal is to investigate, find clues, and solve the mystery before the case goes cold.\n");
+    printf("Good luck, Detective!\n");
 
+    while (!gameOver) {
+        puts("\nWhat would you like to do?");
+        puts("1. Investigate the crime scene");
+        puts("2. Interview a witness");
+        puts("3. Analyze evidence in the lab");
+        puts("4. Follow a suspicious lead");
+        puts("5. Close the case");
+        puts("Enter your choice:");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("\nYou arrive at the crime scene. It looks like a robbery gone wrong.\n");
+                int sceneClue = randomOutcome();
+                if (sceneClue == 0) {
+                    printf("You find a fingerprint on the window ledge. It's a lead!\n");
+                    clues[cluesFound] = "Fingerprint on the window ledge";
+                    cluesFound++;
+                } else {
+                    printf("You search the area but find nothing useful.\n");
+                }
+                break;
+
+            case 2:
+                printf("\nYou interview a witness.\n");
+                int witnessClue = randomOutcome();
+                if (witnessClue == 0) {
+                    printf("The witness remembers seeing a red car near the scene!\n");
+                    clues[cluesFound] = "Witness saw a red car";
+                    cluesFound++;
+                } else {
+                    printf("The witness is too shaken to recall any helpful details.\n");
+                }
+                break;
+
+            case 3:
+                printf("\nYou head to the forensics lab to analyze the evidence.\n");
+                int labClue = randomOutcome();
+                if (labClue == 0) {
+                    printf("The fingerprint matches a known criminal from previous cases!\n");
+                    clues[cluesFound] = "Fingerprint matched a known criminal";
+                    cluesFound++;
+                } else {
+                    printf("The evidence is inconclusive. You'll need more clues to make a breakthrough.\n");
+                }
+                break;
+
+            case 4:
+                printf("\nYou decide to follow up on the lead about the red car.\n");
+                int leadOutcome = randomOutcome();
+                if (leadOutcome == 0) {
+                    printf("The car is traced back to a warehouse in the industrial district. You find more evidence linking it to the suspect!\n");
+                    clues[cluesFound] = "Red car traced to warehouse";
+                    cluesFound++;
+                } else {
+                    printf("The lead goes cold. No further information is uncovered.\n");
+                }
+                break;
+
+            case 5:
+                if (cluesFound >= 3) {
+                    printf("\nYou have gathered enough clues to close the case.\n");
+                    printf("The criminal has been apprehended, and the case is now solved. Great job, Detective!\n");
+                    mysterySolved = 1;
+                } else {
+                    printf("You don't have enough evidence to close the case. Keep investigating.\n");
+                    displayClues(clues, cluesFound);
+                }
+                break;
+
+            default:
+                printf("\nInvalid choice. Please try again.\n");
+                break;
+        }
+
+        if (mysterySolved) {
+            gameOver = 1;
+        }
+    }
+}
+
+int randomOutcome() {
+    return rand() % 2;
+}
+
+void displayClues(char *clues[], int cluesFound) {
+    printf("\nClues collected so far:\n");
+    if (cluesFound == 0) {
+        printf("No clues found yet. Keep investigating!\n");
+    } else {
+        for (int i = 0; i < cluesFound; i++) {
+            printf("- %s\n", clues[i]);
+        }
+    }
+}
